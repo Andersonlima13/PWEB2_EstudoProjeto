95aefda1880766b01deab6b246e9d1d2a5402194
Andersonlima13 <anderson.lima18pb@gmail.com>
2026-03-04 16:04:18 -0300

    add : ajustando pk em models

--- demo/src/main/java/pweb/demo/model/Corrida.java
+++ demo/src/main/java/pweb/demo/model/Corrida.java
@@ -3,6 +3,7 @@ package pweb.demo.model;
 import java.util.List;
 
 public class Corrida {
+    private long id;
     private Integer tempo;
     private List<Pergunta> perguntas; // nesse caso, vai ter um array de perguntas (questoes) que ja sao predefinidas
     private String titulo;
--- demo/src/main/java/pweb/demo/model/Lobby.java
+++ demo/src/main/java/pweb/demo/model/Lobby.java
@@ -3,6 +3,11 @@ package pweb.demo.model;
 import java.util.List;
 
 public class Lobby {
+    private List<Participante> participantes; // em teoria , todos os participantes comecam no lobby
     private List<Corrida> corridas;
+
+    public List<Corrida> getCorridas() {
+        return corridas;
+    }
 
 }
