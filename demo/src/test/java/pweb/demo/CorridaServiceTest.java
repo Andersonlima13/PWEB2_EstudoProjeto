647fa2b81a2de0761a1ab79d3405dc635548b552
Andersonlima13 <anderson.lima18pb@gmail.com>
2026-03-25 11:50:30 -0300

    add : testes em corrida

--- demo/src/test/java/pweb/demo/CorridaServiceTest.java
+++ demo/src/test/java/pweb/demo/CorridaServiceTest.java
@@ -28,4 +28,61 @@ public class CorridaServiceTest {
 
     }
 
+    @Test
+    void deveFalharAoCriarCorridaSemTitulo() {
+        CorridaService corridaService = new CorridaService();
+
+        CorridaDto dto = new CorridaDto();
+        dto.setTempo(12);
+        dto.setDescricao("Esse é um teste da corrida sem título");
+
+        try {
+            corridaService.criar(dto);
+        } catch (IllegalArgumentException e) {
+            // Uma boa maneira de evitar erros é criar uma classe especifica para o erro de
+            // titulo
+            assertEquals("Título é obrigatório", e.getMessage());
+        } // evitando falsos negativos por conta do erro nao ser igual
+    }
+
+    @Test
+    void deveFalharAoCriarCorridaComTempoNegativo() {
+        CorridaService corridaService = new CorridaService();
+        CorridaDto dto = new CorridaDto();
+        dto.setTitulo("Corrida com tempo negativo");
+        dto.setTempo(-5);
+        dto.setDescricao("Esse é um teste da corrida com tempo negativo");
+        try {
+            corridaService.criar(dto);
+        } catch (IllegalArgumentException e) {
+            assertEquals("Tempo deve ser positivo", e.getMessage());
+        }
+    }
+
+    @Test
+    void deveFalharAoCriarCorridaSemDescricao() {
+        CorridaService corridaService = new CorridaService();
+        CorridaDto dto = new CorridaDto();
+        dto.setTitulo("Corrida sem descrição");
+        dto.setTempo(10);
+        try {
+            corridaService.criar(dto);
+        } catch (IllegalArgumentException e) {
+            assertEquals("Descrição é obrigatória", e.getMessage());
+        }
+    }
+
+    @Test
+    void deveFalharAoCriarCorridaSemTempo() {
+        CorridaService corridaService = new CorridaService();
+        CorridaDto dto = new CorridaDto();
+        dto.setTitulo("Corrida sem tempo");
+        dto.setDescricao("Esse é um teste da corrida sem tempo");
+        try {
+            corridaService.criar(dto);
+        } catch (IllegalArgumentException e) {
+            assertEquals("Tempo é obrigatório", e.getMessage());
+        }
+    }
+
 }
